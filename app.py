84dd038bd7edcd6bad7f7f092e6e1a484c3a0ddf
hkrobotics <kumarhemant24jan@gmail.com>
2022-05-12 21:02:39 +0530

    ⬆️ Update Title

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
-st.title("Laptop Predictor")
+st.title("Laptop Price Predictor by Piyush Hule ")
 
 # brand
 company = st.selectbox('Brand',df['Company'].unique())
@@ -33,15 +33,15 @@ screen_size = st.number_input('Screen Size')
 resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
 
 #cpu
-cpu = st.selectbox('CPU',df['Cpu brand'].unique())
-
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+cpu = st.selectbox('CPU Brand',df['Cpu brand'].unique())
 
 ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
 
-gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
 
-os = st.selectbox('OS',df['os'].unique())
+gpu = st.selectbox('GPU Brand',df['Gpu brand'].unique())
+
+os = st.selectbox('Operating System',df['os'].unique())
 
 if st.button('Predict Price'):
     # query
